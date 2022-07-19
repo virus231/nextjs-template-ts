7e59fbfc9f2da7be21155f895eb07507d5f4e899
Vlad Prodan <vladprodan6@gmail.com>
2022-07-19 19:43:30 +0200

    :rocket: config TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./app/components/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#FF7652",
+				purple: "#6C5ECF",
+				"light-blue": "#32A8E2",
+				gray: {
+					400: "#9898ad",
+					500: "#8c8ca4",
+					600: "#707081",
+					700: "#55556 2",
+					800: "#353340",
+					900: "#272532"
+				}
+			}
+		}
+	},
+	plugins: []
 }
