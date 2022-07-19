b68ddd22d9332b6f6b126ac2c0a7182c5dadf167
Vlad Prodan <vladprodan6@gmail.com>
2022-07-19 19:27:41 +0200

    :rocket: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	poweredByHeader: false,
+	env: {
+		API_URL: process.env.REACT_APP_URL
+	},
+	images: {
+		domains: ["localhost"]
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "http://localhost:3000/api/:path*"
+			}
+		]
+	}
 }
 
 module.exports = nextConfig
